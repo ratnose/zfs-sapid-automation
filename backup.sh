c34c429cab9611fb36c2fa4553692fe7556d88ff
Mattias Hedman <mattias.hedman@me.com>
2019-12-11 14:05:51 +0100

    Small messages changes

--- backup.sh
+++ backup.sh
@@ -3,9 +3,9 @@
 date=$(date '+%Y-%m-%d')
 linenumber=1
 zfs list -H -t snapshot -o name | grep -i $date | while read line; do
-    echo "$line"
     zfs send ${line} | gzip > /mnt/snapshots/${date}-${linenumber}.gz
     ((linenumber++))
+    echo "$line > /mnt/snapshops/$date-$linenumber.gz"
 done
 #sudo zfs send ${result} | gzip > /mnt/snapshots/${date}.gz
 echo "Done!"
